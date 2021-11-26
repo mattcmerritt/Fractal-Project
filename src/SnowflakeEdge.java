3690e59d99e6439cb0d4d26fbb4465926b363e9a
mattcmerritt <mattcmerritt@gmail.com>
2021-11-26 01:51:31 -0500

    restructured edges to require less objects

--- src/SnowflakeEdge.java
+++ src/SnowflakeEdge.java
@@ -14,12 +14,29 @@ public class SnowflakeEdge {
     }
 
     public void draw(Graphics g) {
-        drawEdge(g);
+        draw(g, start, end, color);
     }
 
-    public void drawEdge(Graphics g) {
+    public void draw(Graphics g, Point start, Point end, Color color) {
         g.setColor(color);
-        g.drawLine((int) Math.round(start.getX()), (int) Math.round(start.getY()), (int) Math.round(end.getX()), (int) Math.round(end.getY()));
+
+        int startX = (int) Math.round(start.getX());
+        int startY = (int) Math.round(start.getY());
+
+        int endX = (int) Math.round(end.getX());
+        int endY = (int) Math.round(end.getY());
+        
+        double length = Math.sqrt((endX - startX) * (endX - startX) + (endY - startY) * (endY - startY));
+
+        if (length < 2) {
+            g.drawLine((int) Math.round(start.getX()), (int) Math.round(start.getY()), (int) Math.round(end.getX()), (int) Math.round(end.getY()));
+        }
+        else {
+            g.drawLine((int) Math.round(start.getX()), (int) Math.round(start.getY()), (int) Math.round(end.getX()), (int) Math.round(end.getY()));
+            // draw line from start to one third point
+            // draw two edges
+            // draw line from two thirds point to end
+        }
     }
 
 }
