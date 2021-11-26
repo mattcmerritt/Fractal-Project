9e5f5047e0e1cc9bdf42a39381f3118b60028681
mattcmerritt <mattcmerritt@gmail.com>
2021-11-26 05:30:28 -0500

    added controls to adjust rotation speed

--- src/App.java
+++ src/App.java
@@ -2,9 +2,12 @@ import java.awt.Point;
 import java.awt.Graphics;
 import java.awt.Color;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 import java.awt.event.ActionEvent;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class App {
 
@@ -12,6 +15,12 @@ public class App {
     private JPanel panel;
 
     private Timer timer;
+
+    private double rotationSpeed = 0.05;
+    private JLabel label;
+    private JSlider slider;
+    private JPanel controlPanel;
+    private DecimalFormat speedFormatter = new DecimalFormat("0.0");
 
     private KochSnowflake snowflake;
 
@@ -33,11 +42,31 @@ public class App {
             }
         };
 
+        controlPanel = new JPanel();
+
+        label = new JLabel("Rotation Speed: " + speedFormatter.format(rotationSpeed * 100));
+        controlPanel.add(label);
+
+        slider = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+        slider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                rotationSpeed = slider.getValue() * 0.001;
+                if (rotationSpeed < 0.01) {
+                    rotationSpeed = 0;
+                }
+                label.setText("Rotation Speed: " + speedFormatter.format(rotationSpeed * 100));
+            }
+        });
+        controlPanel.add(slider);
+
+        panel.add(controlPanel);
+
         timer = new Timer(25, null);
         timer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                snowflake.rotate(0.05);
+                snowflake.rotate(rotationSpeed);
                 panel.repaint();
             }
         });
